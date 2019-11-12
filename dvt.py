5713e319c5ead9daf33e4b7e92fb6532ccac985d
Edgar Gonzalez <edgar.gonzalez2@mavs.uta.edu>
2019-11-11 17:58:31 -0800

    Finished writeup

--- dvt.py
+++ dvt.py
@@ -1,39 +1,61 @@
+import time
 import sys
 import csv
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+
+#wrapper class, responsible for network initailization paramaters and running the dvt algorith
 class Network:
+    #constructor, takes in a list of src,dest,cost arrays and a root paramater for the gui
     def __init__(self, routers, root):
         self.routers = routers
+        #initialize all network paramaters
         self.initialize_network()
+        #populate nodes list this is gonna be an array with as many unique nodes in the networl
         self.nodes = [Node(routers,self.uniq,x, root) for x in range(1, self.uniq + 1)]
     def initialize_network(self):
+        #count the amount of unique nodes in the set not including the cost of course
         unique_nodes = set()
         for x in self.routers:
             for y in x[:-1]:
                 unique_nodes.add(y)
         self.uniq = len(unique_nodes)
-        self.graph = [[16 for _ in range(self.uniq)] for _ in range(self.uniq)]
+    #the backbone of the algorithm returns True if the system is not stable and False if it is
     def broadcast(self):
         count = 0
+        #loop through the neigbors
         for i, x in enumerate(self.nodes):
             neighbors = x.get_neighbors()
             for j, y in enumerate(neighbors):
+                #if the distance is greater than 0 check their Distance Vector Table(DVT)
                 if y > 0:
                     dvt = self.nodes[j].dvt
+                    #calculate if the current nodes DVT has been changed
+                    #used to check if the system is stable
                     did_change = self.nodes[i].update_dvt(dvt)
                     if did_change:
                         count += 1
         return True if count > 0 else False
+    #helper function to print the system
     def print(self):
         for i,x in enumerate(self.nodes):
             print(f'Node {i + 1}')
             x.print()
+    #this is to start the gui after the network has achieved equilibrium
+    def init_gui(self):
+        for x in self.nodes:
+            x.init_gui()
+#Wrapper class that describes a router
 class Node:
+    #Takes in a list of src,des,cost arrays as well as the node id starting at 1
+    #also the root for the gui
     def __init__(self, routers, uniq,router_id, root):
+        self.root = root
+        #initialize the router table
         self.graph = [[16 for _ in range(uniq)] for _ in range(uniq)]
         self.router_id = router_id
+        #loop through array and update links
         for x in range(len(routers)):
             for y in range(len(routers[x]) - 1):
                 if routers[x][y] == self.router_id:
@@ -42,24 +64,32 @@ class Node:
                     cost = routers[x][2]
                     self.graph[a][b] = cost
                     self.graph[b][a] = cost
+        #initialize the cost of the current nodes self to 0
         self.graph[self.router_id - 1][self.router_id - 1] = 0
         self.dvt = self.graph
-        self.gui = GUI(f"Node {self.router_id}", self, root)
+    #helper function to print the system
     def print(self):
         for x in self.dvt:
             print(x)
     def get_neighbors(self):
+        #loops through the graph array and finds the distance to the neighbors
         neighbors = [0 for x in range(len(self.dvt[0]))]
         for i,x in enumerate(self.dvt[self.router_id - 1]):
             if x < 16:
                 neighbors[i] = x
         return neighbors
+    #initialized the gui
+    def init_gui(self):
+        self.gui = GUI(f"Node {self.router_id}", self, self.root)
     def update_dvt(self, neighbor_table):
+        #loops through the neighbor table and checks to see if there is any nodes whose cost is lower than the
+        #current cost
         did_change = False
         count = 0
         for x in range(len(neighbor_table)):
             for y in range(len(neighbor_table[x])):
                 if neighbor_table[x][y] < self.dvt[x][y] and neighbor_table[x][y] < 16:
+                    #make the graph bi-directional
                     self.dvt[x][y] = neighbor_table[x][y]
                     self.dvt[y][x] = neighbor_table[y][x]
                     did_change = True
@@ -67,8 +97,10 @@ class Node:
                         count += 1
         return True if count > 0 else False
 
+#gui class to hanfle gui stuff
 class GUI:
     def __init__(self, title, node, master = None):
+        #we need this line in order to have multiple windows
         self.master = Toplevel(master)
         self.node = node
         self.master.title(title)
@@ -97,26 +129,31 @@ class GUI:
                 dest = y + 1
                 cost = self.node.dvt[x][y]
                 dvt_tree.insert("", "end", values=(source,dest,cost))
-def askopen(name):
-    name.append(filedialog.askopenfilename())
 
 def main():
     if len(sys.argv) < 2:
-        print("usage: [path to file]")
+        print("usage: [path to input]")
+        return
     root = Tk()
-    filename = []
-    filebtn = Button(root, text="Open input file",command=askopen)
-    filebtn.grid(row=0,column=0)
-    routers = [x for x in csv.reader(open(filename[0]), delimiter=" ")]
+    # read in routers from file
+    routers = [x for x in csv.reader(open(sys.argv[1]), delimiter=" ")]
+    # cast them all to ints
     routers = [list(map(int, router)) for router in routers]
+    #initialize the the network with the values from the file
     topology = Network(routers, root)
     stable = True
     count = 0
+    start = time.time()
+    #boradcast neighbor dvt's until the system reaches equilibirum
     while stable:
         stable = topology.broadcast()
+    end = time.time()
+    topology.init_gui()
+    time_label = Label(root, text=f"Time elapased: {(end-start) * 1000:.4f}ms")
+    time_label.grid(row=0,column=0)
+    stable_label = Label(root, text="Stable", fg="red")
+    stable_label.grid(row=2,column=0)
     topology.print()
     mainloop()
 if __name__ == "__main__":
     main()
-
-
